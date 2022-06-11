71012e41f52aede75381b5c50018aae302c727a1
kuba342 <j.derda010@gmail.com>
2022-06-11 16:11:55 +0200

    docs: presence

--- app/controllers/presences_controller.rb
+++ app/controllers/presences_controller.rb
@@ -1,25 +1,51 @@
 class PresencesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  swagger_controller :presences, 'Presences'
+
   before_action :set_presence, only: %i[ show edit update destroy ]
 
   # GET /presences or /presences.json
+  swagger_api :index do
+    summary 'Returns all presences'
+    notes 'Each object from database'
+  end
   def index
     @presences = Presence.all
   end
 
   # GET /presences/1 or /presences/1.json
+  swagger_api :show do
+    summary 'Returns concrete presence'
+    param :path, :id, :integer, :required, "Presence id"
+    notes 'Returns by id'
+  end
   def show
   end
 
   # GET /presences/new
+  swagger_api :new do
+    summary 'Returns html form to create new presence'
+  end
   def new
     @presence = Presence.new
   end
 
   # GET /presences/1/edit
+  swagger_api :edit do
+    summary 'Returns html form to edit presence by id'
+    param :path, :id, :integer, :required, "presence id"
+  end
   def edit
   end
 
   # POST /presences or /presences.json
+  swagger_api :create do
+    summary 'Create a presence'
+    param :form, "presence[isPresent]", :boolean, :required, "is present?"
+    param :form, "presence[lesson_id]", :integer, :required, "lesson id"
+    param :form, "presence[student]", :integer, :required, "student id"
+  end
   def create
     @presence = Presence.new(presence_params)
 
@@ -35,6 +61,13 @@ class PresencesController < ApplicationController
   end
 
   # PATCH/PUT /presences/1 or /presences/1.json
+  swagger_api :update do
+    summary 'Update a presence'
+    param :path, :id, :integer, :required, "presence id"
+    param :form, "presence[isPresent]", :boolean, :required, "is present?"
+    param :form, "presence[lesson_id]", :integer, :required, "lesson id"
+    param :form, "presence[student]", :integer, :required, "student id"
+  end
   def update
     respond_to do |format|
       if @presence.update(presence_params)
@@ -48,6 +81,10 @@ class PresencesController < ApplicationController
   end
 
   # DELETE /presences/1 or /presences/1.json
+  swagger_api :destroy do
+    summary 'Destroys concrete presence by id'
+    param :path, :id, :integer, :required, "presence id"
+  end
   def destroy
     @presence.destroy
 
