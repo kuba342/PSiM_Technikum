1d4e2a325064ffedb075876b4ee7461bd59f15a4
kuba342 <j.derda010@gmail.com>
2022-06-11 14:05:22 +0200

    docs: animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,25 +1,50 @@
 class AnimalsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  swagger_controller :animals, 'Animals'
+
   before_action :set_animal, only: %i[ show edit update destroy ]
 
   # GET /animals or /animals.json
+  swagger_api :index do
+    summary 'Returns all animals'
+    notes 'Each object from database'
+  end
   def index
     @animals = Animal.all
   end
 
   # GET /animals/1 or /animals/1.json
+  swagger_api :show do
+    summary 'Returns concrete animal'
+    param :path, :id, :integer, :required, "Animal id"
+    notes 'Returns by id'
+  end
   def show
   end
 
   # GET /animals/new
+  swagger_api :new do
+    summary 'Returns html form to create new animal'
+  end
   def new
     @animal = Animal.new
   end
 
   # GET /animals/1/edit
+  swagger_api :edit do
+    summary 'Returns html form to edit animal by id'
+    param :path, :id, :integer, :required, "animal id"
+  end
   def edit
   end
 
   # POST /animals or /animals.json
+  swagger_api :create do
+    summary 'Create an animal'
+    param :form, "animal[code]", :string, :required, "animal code"
+    param :form, "animal[animal_type_id]", :integer, :required, "animal type id"
+  end
   def create
     @animal = Animal.new(animal_params)
 
@@ -35,6 +60,12 @@ class AnimalsController < ApplicationController
   end
 
   # PATCH/PUT /animals/1 or /animals/1.json
+  swagger_api :update do
+    summary 'Update an animal'
+    param :path, :id, :integer, :required, "animal id"
+    param :form, "animal[code]", :string, :required, "animal code"
+    param :form, "animal[animal_type_id]", :integer, :required, "animal type id"
+  end
   def update
     respond_to do |format|
       if @animal.update(animal_params)
@@ -48,6 +79,10 @@ class AnimalsController < ApplicationController
   end
 
   # DELETE /animals/1 or /animals/1.json
+  swagger_api :destroy do
+    summary 'Destroys concrete animal by id'
+    param :path, :id, :integer, :required, "animal id"
+  end
   def destroy
     @animal.destroy
 
