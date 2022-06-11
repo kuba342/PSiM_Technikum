cd5d00f2e971a86d91166c1d827213fe3e7d9a5b
kuba342 <j.derda010@gmail.com>
2022-06-11 14:34:39 +0200

    docs: course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,25 +1,49 @@
 class CoursesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  swagger_controller :courses, 'Courses'
+
   before_action :set_course, only: %i[ show edit update destroy ]
 
   # GET /courses or /courses.json
+  swagger_api :index do
+    summary 'Returns all courses'
+    notes 'Each object from database'
+  end
   def index
     @courses = Course.all
   end
 
   # GET /courses/1 or /courses/1.json
+  swagger_api :show do
+    summary 'Returns concrete course'
+    param :path, :id, :integer, :required, "course id"
+    notes 'Returns by id'
+  end
   def show
   end
 
   # GET /courses/new
+  swagger_api :new do
+    summary 'Returns html form to create new course'
+  end
   def new
     @course = Course.new
   end
 
   # GET /courses/1/edit
+  swagger_api :edit do
+    summary 'Returns html form to edit course by id'
+    param :path, :id, :integer, :required, "course id"
+  end
   def edit
   end
 
   # POST /courses or /courses.json
+  swagger_api :create do
+    summary 'Create a course'
+    param :form, "course[name]", :string, :required, "name of the course"
+  end
   def create
     @course = Course.new(course_params)
 
@@ -35,6 +59,11 @@ class CoursesController < ApplicationController
   end
 
   # PATCH/PUT /courses/1 or /courses/1.json
+  swagger_api :update do
+    summary 'Update a course'
+    param :path, :id, :integer, :required, "course id"
+    param :form, "course[name]", :string, :required, "name of the course"
+  end
   def update
     respond_to do |format|
       if @course.update(course_params)
@@ -48,6 +77,10 @@ class CoursesController < ApplicationController
   end
 
   # DELETE /courses/1 or /courses/1.json
+  swagger_api :destroy do
+    summary 'Destroys concrete course by id'
+    param :path, :id, :integer, :required, "course id"
+  end
   def destroy
     @course.destroy
 
