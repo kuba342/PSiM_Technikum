175a69056f2585eee4d17be4ff12c6fe5a2748c7
kuba342 <j.derda010@gmail.com>
2022-06-11 14:19:36 +0200

    docs User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,52 @@
 class UsersController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  swagger_controller :users, 'Users'
+
   before_action :set_user, only: %i[ show edit update destroy ]
 
   # GET /users or /users.json
+  swagger_api :index do
+    summary 'Returns all users'
+    notes 'Each object from database'
+  end
   def index
     @users = User.all
   end
 
   # GET /users/1 or /users/1.json
+  swagger_api :show do
+    summary 'Returns concrete user'
+    param :path, :id, :integer, :required, "user id"
+    notes 'Returns by id'
+  end
   def show
   end
 
   # GET /users/new
+  swagger_api :new do
+    summary 'Returns html form to create new user'
+  end
   def new
     @user = User.new
   end
 
   # GET /users/1/edit
+  swagger_api :edit do
+    summary 'Returns html form to edit user by id'
+    param :path, :id, :integer, :required, "user id"
+  end
   def edit
   end
 
   # POST /users or /users.json
+  swagger_api :create do
+    summary 'Create a user'
+    param :form, "user[login]", :string, :required, "User login"
+    param :form, "user[email]", :string, :required, "User email"
+    param :form, "user[password]", :string, :required, "User password"
+    param :form, "user[password_confirmation]", :string, :required, "User password_confirmation"
+  end
   def create
     @user = User.new(user_params)
 
@@ -35,6 +62,14 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1 or /users/1.json
+  swagger_api :update do
+    summary 'Update a user'
+    param :path, :id, :integer, :required, "user id"
+    param :form, "user[login]", :string, :required, "User login"
+    param :form, "user[email]", :string, :required, "User email"
+    param :form, "user[password]", :string, :required, "User password"
+    param :form, "user[password_confirmation]", :string, :required, "User password_confirmation"
+  end
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -48,6 +83,10 @@ class UsersController < ApplicationController
   end
 
   # DELETE /users/1 or /users/1.json
+  swagger_api :destroy do
+    summary 'Destroys concrete user by id'
+    param :path, :id, :integer, :required, "user id"
+  end
   def destroy
     @user.destroy
 
