6bdd8d1e71c3c2ce754eb89e965c950444f3730d
kuba342 <j.derda010@gmail.com>
2022-06-11 15:03:04 +0200

    docs: student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,25 +1,52 @@
 class StudentsController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+  swagger_controller :students, 'Students'
+
   before_action :set_student, only: %i[ show edit update destroy ]
 
   # GET /students or /students.json
+  swagger_api :index do
+    summary 'Returns all Students'
+    notes 'Each object from database'
+  end
   def index
     @students = Student.all
   end
 
   # GET /students/1 or /students/1.json
+  swagger_api :show do
+    summary 'Returns concrete student'
+    param :path, :id, :integer, :required, "Student id"
+    notes 'Returns by id'
+  end
   def show
   end
 
   # GET /students/new
+  swagger_api :new do
+    summary 'Returns html form to create new student'
+  end
   def new
     @student = Student.new
   end
 
   # GET /students/1/edit
+  swagger_api :edit do
+    summary 'Returns html form to edit student by id'
+    param :path, :id, :integer, :required, "student id"
+  end
   def edit
   end
 
   # POST /students or /students.json
+  swagger_api :create do
+    summary 'Create a student'
+    param :form, "student[firstName]", :string, :required, "student's first name"
+    param :form, "student[lastName]", :string, :required, "student's last name"
+    param :form, "student[class_of_student_id]", :integer, :required, "class of students id"
+    param :form, "student[user_id]", :integer, :required, "User id"
+  end
   def create
     @student = Student.new(student_params)
 
@@ -35,6 +62,14 @@ class StudentsController < ApplicationController
   end
 
   # PATCH/PUT /students/1 or /students/1.json
+  swagger_api :update do
+    summary 'Update a student'
+    param :path, :id, :integer, :required, "student id"
+    param :form, "student[firstName]", :string, :required, "student's first name"
+    param :form, "student[lastName]", :string, :required, "student's last name"
+    param :form, "student[class_of_student_id]", :integer, :required, "class of students id"
+    param :form, "student[user_id]", :integer, :required, "User id"
+  end
   def update
     respond_to do |format|
       if @student.update(student_params)
@@ -48,6 +83,10 @@ class StudentsController < ApplicationController
   end
 
   # DELETE /students/1 or /students/1.json
+  swagger_api :destroy do
+    summary 'Destroys concrete student by id'
+    param :path, :id, :integer, :required, "student id"
+  end
   def destroy
     @student.destroy
 
